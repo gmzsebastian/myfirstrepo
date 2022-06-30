471672b1dab36a71d4a5ad0888dee576eb2f2277
Sebastian Gomez <sgomez@stsci.edu>
2022-06-30 15:52:01 -0400

    useful

--- temperature.py
+++ temperature.py
@@ -2,9 +2,5 @@ import numpy as np
 
 def f_to_c(f):
 	# This function converts fahrenheit to celsius 
-<<<<<<< HEAD
 	c = (f - 32) * 5 / 9
 	return c
-=======
-	return (f - 32) * 5 / 9 + 0
->>>>>>> main
